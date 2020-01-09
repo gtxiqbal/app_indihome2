442077304d08085aaa521521500d7f19583d04ab
iqbal.alhusain <iqbal.alhusain@collega.co.id>
2020-01-09 16:30:33 +0700

    app_indihomev2

---
+++ src/test/java/com/gtx/app_indihomev2/AppIndihomev2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gtx.app_indihomev2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppIndihomev2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
